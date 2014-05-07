2d6f5f8cb497322af9b07227b0d351b1f89e4f63
Itai Koren <itkoren@gmail.com>
2014-05-07 22:13:26 +0300

    Initial commit for exercise 3
    Signed-off-by: Itai Koren <itkoren@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Lets-Node-ex-3
-==============
+Third Exercise (package.json)
+=================================================
 
-Lets Node Exercise 3
+Please complete the following steps:
+ 
+1. Create a package.json for the "sentiment" module
+2. Create a new TCP server in a separated project
+3. Create a package.json for the new TCP server
+3. Link the sentiment to the new TCP server and use it in the project
+
+#####Use the sentiment directory in this repository as a starting point for your sentiment module
+#####*You can also use the boilerplate index.js as a staring point for your server code*
---
+++ sentiment/index.js
@@ -0,0 +1,53 @@
+// Include The 'require.async' Module
+require("require.async")(require);
+
+/**
+ * Tokenizes an input string.
+ *
+ * @param {String} Input
+ *
+ * @return {Array}
+ */
+function tokenize (input) {
+    return input
+            .replace(/[^a-zA-Z ]+/g, "")
+            .replace("/ {2,}/", " ")
+            .toLowerCase()
+            .split(" ");
+}
+
+/**
+ * Performs sentiment analysis on the provided input "term".
+ *
+ * @param {String} Input term
+ *
+ * @return {Object}
+ */
+module.exports = function (term, callback) {
+	// Split line term to letters only words array
+	var words = tokenize(term || "");
+	var score = 0;
+	var word;
+	var rate;
+	var i;
+
+	// Include the AFINN Dictionary JSON asynchronously
+	require.async("./AFINN.json", function(afinn) {
+		console.log("LOADED!!!");
+		// Iterate through the words
+		for (i = 0; i < words.length; i++) {
+			word = words[i];
+			
+			// Get the word rate from the AFINN dictionary
+			rate = afinn[word];
+			if (rate) { 
+				// Add current word rate to the final calculated sentiment score
+				score += rate;
+			}
+		}
+		
+		setTimeout(function() {
+			callback(null, score);
+		}, 0);
+	});
+};
